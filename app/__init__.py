49bd7cbf95c54b99329e341efaa004e58c6274e5
vcoetoG <vcoeto@hotmail.com>
2021-06-11 14:52:29 -0500

    solved master merge

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About", url=os.getenv("URL"))
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact", url=os.getenv("URL"))
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="Projects", url=os.getenv("URL"))
