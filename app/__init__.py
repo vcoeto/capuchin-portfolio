ecfbfc3f5b42224aba4efd50a9bc259bab68d98f
vcoetoG <vcoeto@hotmail.com>
2021-06-27 13:22:28 -0500

    added login and register

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 import os
+from types import MethodDescriptorType
 from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-
 load_dotenv()
 
 app = Flask(__name__)
 app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 db.init_app(app)
+from app.db import get_db
 
 
 @app.route('/')
@@ -28,6 +30,60 @@ def contact():
 @app.route('/projects')
 def projects():
     return render_template('projects.html', title="Projects", url=os.getenv("URL"))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    return "Register Page not yet implemented", 501
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
 
 @app.route('/health')
 def health():
