f7ff5c365bdc6c125e7ae85dae2cf9a1cbe45e73
vcoetoG <vcoeto@hotmail.com>
2021-07-15 18:32:30 -0500

    test for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -55,9 +55,9 @@ def index():
     return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
 
 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html', title="About", url=os.getenv("URL"))<
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About", url=os.getenv("URL"))
 
 
 @app.route("/contact")
