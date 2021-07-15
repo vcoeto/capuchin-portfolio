f778a443c66f5576649919518bf4e664c2924471
vcoetoG <vcoeto@hotmail.com>
2021-07-15 18:40:53 -0500

    discord messages, test bash and print containers

---
+++ prod-test.sh
@@ -0,0 +1,14 @@
+domain='https://vcoeto.duckdns.org'
+endpoints=("/" "/register" "/login" "/contact" "projects" "/about/yenyu" "/health")
+error=0
+for endpoint in "${endpoints[@]}"
+do
+   url="${domain}${endpoint}"
+   status=$(curl -s -o /dev/null -w "%{http_code}" "${url}")
+   printf "#### Testing route: ${endpoint} -> Status: ${status} ####\n"
+   if [[ $status -ne 200 ]]
+   then
+       error=1
+   fi  
+done
+exit $error
